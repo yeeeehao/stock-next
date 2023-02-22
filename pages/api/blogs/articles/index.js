5d8d0cb83ced69b4908faa48f83d3c9141374bd6
769399481 <86551711+769399481@users.noreply.github.com>
2023-02-22 16:05:57 +0700

    blog - connect&delete

--- pages/api/blogs/articles/index.js
+++ pages/api/blogs/articles/index.js
@@ -4,23 +4,18 @@ const connectionString =
 
 export default async function handler(req, res) {
   await connect(connectionString);
-
   console.log("req.method: ", req.method);
 
   if (req.method === "GET") {
     const docs = await Article.find();
-
     res.status(200).json(docs);
   } else if (req.method === "POST") {
-    console.log(req.body);
-
-    // res.status(200).json(req.body);
-
+    console.log(typeof req.body);
+    // res.status(200).json(req.body)
     const doc = await Article.create(req.body);
     res.status(201).json(doc);
   } else {
     res.setHeader("Allow", ["GET", "POST"]);
-
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
@@ -31,5 +26,4 @@ const articleSchema = new Schema({
 });
 
 console.log("Mongoose Models", models);
-
 const Article = models?.article || model("article", articleSchema);
---
+++ pages/blogs/index.jsx
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Home({ blogs }) {
+  function deleteBlog(id) {
+    fetch(`http://localhost:3000/api/blogs/articles/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // alert("Deleting " + id)
+        window.location.reload(false);
+      });
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Blogs</title>
+      </Head>
+      <h1>Blogs</h1>
+      <table>
+        <tbody>
+          {blogs.map((blog) => {
+            return (
+              <tr key={blog._id}>
+                <td>
+                  <Link href={`/blogs/${blog._id}`}>{blog.title}</Link>
+                </td>
+                <td>
+                  <button onClick={() => deleteBlog(blog._id)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <p></p>
+    </>
+  );
+}
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/blogs/articles/`);
+  const blogs = await res.json();
+  // console.debug('blog 1', blogs)
+  return { props: { blogs } };
+}
