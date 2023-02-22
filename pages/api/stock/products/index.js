cd3d4561ee35bd9581cbeb582d007062c66f98cd
769399481 <86551711+769399481@users.noreply.github.com>
2023-02-22 16:44:05 +0700

    test

--- pages/api/stock/products/index.js
+++ pages/api/stock/products/index.js
@@ -4,18 +4,23 @@ const connectionString =
 
 export default async function handler(req, res) {
   await connect(connectionString);
+
   console.log("req.method: ", req.method);
 
   if (req.method === "GET") {
     const docs = await Product.find();
+
     res.status(200).json(docs);
   } else if (req.method === "POST") {
-    console.log(typeof req.body);
-    // res.status(200).json(req.body)
+    console.log(req.body);
+
+    // res.status(200).json(req.body);
+
     const doc = await Product.create(req.body);
     res.status(201).json(doc);
   } else {
     res.setHeader("Allow", ["GET", "POST"]);
+
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
@@ -27,4 +32,5 @@ const productSchema = new Schema({
 });
 
 console.log("Mongoose Models", models);
-const Product = models?.product || model("product", productSchema);
+
+const Product = models?.product || model("products", productSchema);
