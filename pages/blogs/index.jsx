13fa657a4bc053026bccf1e3470d76936a4ca85a
769399481 <86551711+769399481@users.noreply.github.com>
2023-03-01 14:58:50 +0700

    change

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -44,7 +44,9 @@ export default function Home({ blogs }) {
   );
 }
 export async function getServerSideProps() {
-  const res = await fetch(`http://localhost:3000/api/blogs/articles/`);
+  const res = await fetch(
+    `https://stock-next-yeeeehao.vercel.app/api/blogs/articles/`
+  );
   const blogs = await res.json();
   // console.debug('blog 1', blogs)
   return { props: { blogs } };
